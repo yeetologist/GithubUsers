1050e9b6e3e8fa6be1f5f9f9676ac341df1495dd
yeetologist <71173561+yeetologist@users.noreply.github.com>
2023-09-14 22:52:44 +0700

    Change HttpLoggingInterceptor level to none

--- app/src/main/java/com/github/yeetologist/githubusers/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/github/yeetologist/githubusers/data/retrofit/ApiConfig.kt
@@ -9,7 +9,7 @@ class ApiConfig {
     companion object {
         fun getApiService(): ApiService {
             val loggingInterceptor =
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build()
