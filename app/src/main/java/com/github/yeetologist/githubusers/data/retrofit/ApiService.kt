e7c022ee80bc0e63db5b0952b30526f8777b47db
yeetologist <71173561+yeetologist@users.noreply.github.com>
2023-09-16 08:13:27 +0700

    Change expired personal access token

--- app/src/main/java/com/github/yeetologist/githubusers/data/retrofit/ApiService.kt
+++ app/src/main/java/com/github/yeetologist/githubusers/data/retrofit/ApiService.kt
@@ -11,27 +11,27 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: Bearer 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")
+    @Headers("Authorization: Bearer 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")
     fun getSearch(
         @Query("q") username: String,
         @Query("page") page: Int = 1
     ): Call<SearchResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: Bearer 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")
+    @Headers("Authorization: Bearer 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")
     fun getDetail (
         @Path("username") username: String
     ) : Call<DetailUserResponse>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: Bearer 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")
+    @Headers("Authorization: Bearer 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")
     fun getFollowing (
         @Path("username") username: String,
         @Query("page") page: Int = 1
     ) : Call<List<FollowUserResponseItem>>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: Bearer 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")
+    @Headers("Authorization: Bearer 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")
     fun getFollowers (
         @Path("username") username: String,
         @Query("page") page: Int = 1
